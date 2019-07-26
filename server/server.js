8bff84d169b964fc7183e9ac90fa0f200410d479
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-07-26 12:37:48 +0400

    login user and create token

--- server/server.js
+++ server/server.js
@@ -34,6 +34,38 @@ app.post("/api/users/register", (req, res) => {
   });
 });
 
+app.post("/api/users/login", (req, res) => {
+  // find the email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found"
+      });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "Wrong password" });
+      }
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res
+          .cookie("w_auth", user.token)
+          .status(200)
+          .json({
+            loginSuccess: true
+          });
+      });
+    });
+  });
+
+  // check the password
+  // generate a token
+});
+
 //---------- end of Users ----------
 
 const port = process.env.PORT || 3002;
