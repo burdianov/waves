7b6a6c19d9c45387e00e62f7aa7b239365a14685
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-07-26 20:59:35 +0400

    adjust proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return <div className="App">My app</div>;
+class App extends Component {
+  componentDidMount() {
+    axios.get("/api/product/brands").then(response => {
+      console.log(response);
+    });
+  }
+
+  render() {
+    return <div className="App">My app</div>;
+  }
 }
 
 export default App;
