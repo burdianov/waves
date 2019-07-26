499664aa7f0a6938e972c21e36fb4cae5041cdff
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-07-26 15:15:57 +0400

    adding products

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 const { User } = require("./models/user");
 const { Brand } = require("./models/brand");
 const { Wood } = require("./models/wood");
+const { Product } = require("./models/product");
 
 //---------- end of models ----------
 
@@ -31,6 +32,22 @@ const { auth } = require("./middleware/auth");
 const { admin } = require("./middleware/admin");
 
 //---------- end of middleware ----------
+
+//---------- Products ----------
+
+app.post("/api/product/article", auth, admin, (req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      article: doc
+    });
+  });
+});
+
+//---------- end of Products ----------
 
 //---------- Woods ----------
 
