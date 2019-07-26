564ae5536fe49b9fcab7b9081bc3ad264a2b8bc1
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-07-26 13:56:34 +0400

    user logout

--- server/server.js
+++ server/server.js
@@ -83,6 +83,23 @@ app.post("/api/users/login", (req, res) => {
   });
 });
 
+app.get("/api/user/logout", auth, (req, res) => {
+  User.findOneAndUpdate(
+    {
+      _id: req.user._id
+    },
+    {
+      token: ""
+    },
+    (err, doc) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      });
+    }
+  );
+});
+
 //---------- end of Users ----------
 
 const port = process.env.PORT || 3002;
