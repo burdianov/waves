d34372da7c4fa8d2a83e2a65a9ca027afd5f2a54
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-07-26 08:39:50 +0400

    create User model

--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+//---------- models ----------
+
+const { User } = require("./models/user");
+
+//---------- end of models ----------
+
+//---------- Users ----------
+
+app.post("/api/users/register", (req, res) => {
+  res.send(200);
+});
+
+//---------- end of Users ----------
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
